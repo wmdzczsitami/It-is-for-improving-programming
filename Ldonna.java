367933a283d32fe5830b39b4533d9b354bfe72f0
Itami <robertwqp4111@gmail.com>
2026-02-19 23:34:38 +0900

    Refactor card function and improve character creation
    Refactor card probability assignment and enhance character creation with name input.

--- Ldonna.java
+++ Ldonna.java
@@ -3,21 +3,20 @@ print("hello world")
 
 ->5개의 카드중 확률 랜덤 부여, 10.0%~50.0%
 fn=card():list<:float>{
-  of prob:list = which(x5:float)
+  of prob:list = which:float(it_amount(5))
   /**of = 변수 선언,
   which = null과 비슷, 타입 지정 가능, 위 코드를
-  of prob:list<x5:float> = which() 로도 가능
-  */
+  of prob:list<:float> = which() 로도 가능
   return prob.foreach { i
-    prob[i] = randomfloat(10.0, 50.0)
+    prob[i] = random_float(10.0, 50.0)
   }
 }
 
-->대미지, 체력, 능력등을 가진 캐릭터 생성 법
-fn=character(which()) return itself
-fn=summon(of param = which()) {
-  if param.have(null or shuffle) return
-  character(param.fix())
+->대미지, 체력, 능력등을 가진 캐릭터에
+이름 생성칸 넣기, 함수 작동
+fn=character(which:list()) {
+  it.add_in_list(of name:string =null)
+  set_name(it.fix)
 }
 
 
