3510905f25456aa51855f6f1a68db661f8e1c222
Itami <robertwqp4111@gmail.com>
2026-02-19 23:22:36 +0900

    Update Ldonna.java

--- Ldonna.java
+++ Ldonna.java
@@ -1,22 +1,23 @@
 ->hello world
-h.d='print(hello world)'
-/**h.d = 쉼표 안 즉시 실행*/
+print("hello world")
 
-->5개의 카드중 확률 랜덤 부여, 총합 100%,
-가장 낮은 확률과 높은 확률의 최대 차이가 10미만
-func = card():list<:float>{
+->5개의 카드중 확률 랜덤 부여, 10.0%~50.0%
+fn=card():list<:float>{
   of prob:list = which(x5:float)
   /**of = 변수 선언,
   which = null과 비슷, 타입 지정 가능, 위 코드를
   of prob:list<x5:float> = which() 로도 가능
   */
-  h.d='
-      for (i in 4) {
-  prob[0] = randomFloat(12.0, 24.0)
+  return prob.foreach { i
+    prob[i] = randomfloat(10.0, 50.0)
+  }
+}
 
-
-
-
-
+->대미지, 체력, 능력등을 가진 캐릭터 생성 법
+fn=character(which()) return itself
+fn=summon(of param = which()) {
+  if param.have(null or shuffle) return
+  character(param.fix())
+}
 
 
